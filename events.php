786fa37f865735f3c43f0243a3aa7492798c7644
444Duarte <duartemr.pinto@gmail.com>
2015-11-23 20:04:02 +0000

    EVENTS-Added left bar

--- events.php
+++ events.php
@@ -6,19 +6,29 @@
 	<script src="dist/sweetalert.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="dist/sweetalert.css">
 	<link rel="stylesheet" href="css/events.css">
-	<link rel="stylesheet" href="css/style.css">
 
 </head>
 <body>
 	<div class="top-bar">
-		<form id="search">
+		<form>
 			<input id="search-bar" type="text" name="search" required="optional" placeholder="Search"/>
 			<input id="searchButton" type="button" value="Search"/>
 		</form>
 	</div>	
 	
 	<div class="left-bar">
-
+		<div>
+			My Events
+			<br> 
+			<a href="">Event</a>
+		</div>
+		<div>
+			Other Events
+			<br>
+			<a href="">Event</a>
+		</div>
+		
 	</div>
+	
 </body>
 </html>
