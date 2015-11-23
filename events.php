c601cc85b5fb7458ef85bb221dfc3fc7b06ac3cc
444Duarte <duartemr.pinto@gmail.com>
2015-11-23 16:58:52 +0000

    Top bar finished

---
+++ events.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Event Manager</title>
+	<meta charset='UTF-8'>
+	<script src="dist/sweetalert.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="dist/sweetalert.css">
+	<link rel="stylesheet" href="css/events.css">
+	<link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+	<div class="top-bar">
+		<form id="search">
+			<input id="search-bar" type="text" name="search" required="optional" placeholder="Search"/>
+			<input id="searchButton" type="button" value="Search"/>
+		</form>
+	</div>	
+	
+	<div class="left-bar">
+
+	</div>
+</body>
+</html>
