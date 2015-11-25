9231fd1c74b112d09714397519a114488ed65d81
444Duarte <duartemr.pinto@gmail.com>
2015-11-25 13:02:31 +0000

    Faltavam cenas

---
+++ topbar.php
@@ -0,0 +1,15 @@
+<head>
+	<link rel="stylesheet" href="css/topbar.css">
+</head>
+
+<div class="top-bar">
+		<button id="preferences_button" type="button"> <img src="images/template/gear-16.png"></button>
+		<div id="pref_menu">
+			<a href="">Logout</a>
+		</div>
+		<form>
+			<input id="searchButton" type="button" value="Search"/>
+			<input id="search-bar" type="text" name="search" required="optional" placeholder="Search"/>
+		</form>
+
+</div>	
