8096a6098da435ad9a92de28e28258c0523ef7be
tomislaaaav <m_botelho8@hotmail.com>
2015-11-25 19:56:29 +0000

    done

--- js/bridge.js
+++ js/bridge.js
@@ -4,8 +4,8 @@ $('#Reg').submit(function(ev) {
         var username = document.forms["Registo"]["username"].value;
         var password = document.forms["Registo"]["password"].value;
 
-        if(password=="" || username=="") {
-            swal("Oops...", "Something went wrong!", "error");
+        if(password == "" || username == "") {
+            swal("Oops...", "You didn't fill one of the fields.", "error");
             return false;
         }
         
@@ -39,64 +39,43 @@ $('#Reg').submit(function(ev) {
         this.submit(); // If all the validations succeeded
 });
 
-$("#Log").click(function(){
-    swal({   
-        title: "Login",   
-        text: "",   
-        type: "input",
-        showCancelButton: true,   
-        closeOnConfirm: false,   
-        animation: "slide-from-top",   
-        inputPlaceholder: "Username" },
-        function(inputValue){ if (inputValue === false) return false;      
-                if (inputValue === "") {     
-                swal.showInputError("You need to write something!");     
-                return false   
-                }      
-        swal({   
-            title: "Login",   
-            text: "",   
-            type: "input",
-            showCancelButton: true,   
-            closeOnConfirm: false,   
-            animation: "slide-from-top",   
-            inputPlaceholder: "Password" },
-            function(inputValue) {
-                if (inputValue === false)
-                        return false;      
-                            if (inputValue === "") {     
-                                swal.showInputError("You need to write something!");     
-                                return false   
-                            }
-            //verificar se a password coincide com a password do username na base de dados
-            // se coincidir, entrar
-            swal("Nice!", "Password: " + inputValue, "success");
+$("#Log").click(function(ev){
+    ev.preventDefault();
 
-            // se nao coincidir, mostrar inputError 
-        });
-        // se nao existir, mostrar inputError
-        })
+    var username = document.forms["Login"]["username"].value;
+    var password = document.forms["Login"]["password"].value;
+    var repPassword = document.forms["Login"]["repeatPassword"].val;
+
+    if (password == "" || username == "") {
+        swal("Oops...", "Something went wrong!", "error");
+        return false;
+    }
+
+    if (password !== repPassword) {
+        swal("Oops...", "The passwords don't match.", "error");
+        return false;
+    }
+
 
     $.post(
-            '../php/register.php',
-            {
-                'username' : username,
-                'password' : password
-            },
-            function(data) {
-                var response = data['register'];
-                switch(response) {
-                    case 'user_exists':
-                        swal("Login failed, the username already exists.", "Try again.", "insuccess")
-                        break;
-                    case 'success':
-                        swal("Login successfull.", "success")
-                        break;
-                    default:
-                        //displayError("Error while processing the login...");
-                        break;
-                }
-
-
-        });                 
+        '../php/register.php',
+        {
+        'username' : username,
+        'password' : password
+        },
+        function(data) {
+            var response = data['register'];
+            switch(response) {
+                case 'user_exists':
+                    swal("Login failed, the username already exists.", "Try again.", "insuccess")
+                    break;
+                case 'success':
+                    swal("Login successfull.", "success")
+                    break;
+                default:
+                    //displayError("Error while processing the login...");
+                    break;
+        }
+    });
+    this.submit();                 
 });
