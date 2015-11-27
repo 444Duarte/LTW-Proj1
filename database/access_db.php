73c73d4479d868439bdb23a861a928d028c18caf
444Duarte <duartemr.pinto@gmail.com>
2015-11-27 17:18:36 +0000

    cenas

---
+++ database/access_db.php
@@ -0,0 +1,210 @@
+<?php
+	include_once('connect.php');
+	
+	class User {
+		public $idUser = "";
+		public $user = "";
+		public $password = "";
+	}
+
+	class Event {
+		public $idEvent = "";
+		public $datadoEvento = "";
+		public $descricao = "";
+		public $imagem = "";
+	}
+
+	class EventType {
+		public $idEventType = "";
+		public $idEvent = "";
+		public $type = "";
+	}
+
+	class AdminEvent {
+		public $idUser = "";
+		public $idEvent = "";
+	}
+
+	class GoToEvent {
+		public $idUser = "";
+		public $idEvent = "";
+	}
+
+	class Comment {
+		public $idComment = "";
+		public $idUser = "";
+		public $idEvent = "";
+		public $comment = "";
+	}
+
+	function startDB($sting) {
+		$stmt = $db->prepare(sting);
+		$result = $stmt->fetchAll();
+
+		return $result;
+	}
+
+	function retrieveUser() {
+		$result = startDB('SELECT * FROM User;');
+
+		$users = array();
+		foreach( $result as $row) {			
+			$list = array();
+			$list[' '] = $row['idUser'];
+			$list['user'] = $row['user'];
+			$list['password'] = $row['password'];
+
+			array_push($users, $list);
+		}
+
+		echo json_encode($users);
+	}
+
+	function retrieveEvent() {
+		$result = startDB('SELECT * FROM Event;');
+
+		$events = array();
+		foreach( $result as $row) {			
+			$list = array();
+			$list['idEvent'] = $row['idEvent'];
+			$list['datadoEvento'] = $row['datadoEvento'];
+			$list['descricao'] = $row['descricao'];
+			$list['imagem'] = $row['imagem'];
+
+			array_push($events, $list);
+		}
+
+		echo json_encode($events);	
+	}
+
+	function retrieveEventType() {
+		$result = startDB('SELECT * FROM EventType;');
+
+		$eventsType = array();
+		foreach( $result as $row) {			
+			$list = array();
+			$list['idEventType'] = $row['idEventType'];
+			$list['datadoEvento'] = $row['datadoEvento'];
+			$list['descricao'] = $row['descricao'];
+			$list['imagem'] = $row['imagem'];
+
+			array_push($eventsType, $list);
+		}
+
+		echo json_encode($eventsType);	
+	}
+
+	function retrieveAdminEvent() {
+		$result = startDB('SELECT * FROM AdminEvent;');
+
+		$adminEvents = array();
+		foreach( $result as $row) {			
+			$list = array();
+			$list['idUser'] = $row['idUser'];
+			$list['idEvent'] = $row['idEvent'];
+
+			array_push($adminEvents, $list);
+		}
+
+		echo json_encode($adminEvents);	
+	}
+
+	function retrieveGoToEvent() {
+		$result = startDB('SELECT * FROM GoToEvent;');
+
+		$goToEvents = array();
+		foreach( $result as $row) {			
+			$list = array();
+			$list['idUser'] = $row['idUser'];
+			$list['idEvent'] = $row['idEvent'];
+
+			array_push($goToEvents, $list);
+		}
+
+		echo json_encode($goToEvents);	
+	}
+
+	function retrieveComment() {
+		$result = startDB('SELECT * FROM Comment;');
+
+		$comments = array();
+		foreach( $result as $row) {			
+			$list = array();
+			$list['idComment'] = $row['idComment'];
+			$list['idUser'] = $row['idUser'];
+			$list['idEvent'] = $row['idEvent'];
+			$list['comment'] = $row['comment'];
+
+			array_push($comments, $list);
+		}
+
+		echo json_encode($comments);		
+	}
+
+	function retrieveEventsOfAnUser($user_given) {
+		global $db;
+
+		$stmt = $db->prepare('SELECT idUser FROM User where username = :user_given');
+		$stmt->bindParam(':user_given', $user_given, PDO::PARAM_STR);
+		$stmt->execute();
+		$result = $stmt->fetchAll();
+
+		$idUser = $result[0]['idUser'];
+		
+		$stmt = $db->prepare('SELECT idEvent FROM AdminEvent WHERE idUser = :idUser');
+		$stmt->bindParam(':idUser', $idUser, PDO::PARAM_INT);
+		$stmt->execute();
+		$result = $stmt->fetchAll();
+
+		foreach($result as $row) {
+			//dar retrieve dos eventos de AdminEvent
+		}
+
+		$stmt = $db->prepare('SELECT idEvent FROM GoToEvent WHERE idUser = :idUser');
+		$stmt->bindParam(':idUser', $idUser, PDO::PARAM_INT);
+		$stmt->execute();
+		$result = $stmt->fetchAll();
+
+		foreach($result as $row) {
+			//dar retrieve dos eventos de GoToEvent
+		}
+
+		return true;
+	}
+
+	function retrieveEventbyID($idEvent) {
+		global $db;
+
+		$stmt = $db->prepare('SELECT * FROM Event WHERE idEvent = :idEvent');
+		$stmt->bindParam(':idEvent', $idEvent, PDO::PARAM_INT);
+		$stmt->execute();
+
+		$result = $stmt->fetchAll();
+
+		$event = $result[0];
+		$idEventType = 0 /*$event['idEventType']*/;
+
+		$stmt = $db->prepare('SELECT * FROM EventType WHERE idEvent = :idEventType');
+		$stmt->bindParam(':idEventType', $idEventType, PDO::PARAM_INT);
+		$stmt->execute();
+
+		$result = $stmt->fetchAll();
+		$event['type'] = $result[0]['type'];
+
+		echo json_encode($event);
+	}
+
+	function retrieveCommentsByEventID($idEvent) {
+		global $db;
+
+		$stmt = $db->prepare('SELECT * FROM Comment WHERE idEvent = :idEvent');
+		$stmt->bindParam(':idEvent', $idEvent, PDO::PARAM_INT);
+		$stmt->execute();
+
+		$result = $stmt->fetchAll();
+
+		echo json_encode($result);
+	}
+
+	
+?>  
--- templates/perfil.php
+++ templates/perfil.php
@@ -1,36 +1,18 @@
-<header>
-	<link rel="stylesheet" href="css/event.css">
-</header>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Event Manager</title>
+	<meta charset='UTF-8'>
+	<script src="dist/sweetalert.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="dist/sweetalert.css">
+	<link rel="stylesheet" href="css/perfil.css">
 
-<?php
-	include 'database/connect.php';
-	include 'database/access_db.php';
-
-	$event = retrieveEventByID($_GET['event']);
-	
- ?>
-
-<div class="display-event">
-	<div id="header">
-		<div>
-			<h1 id="name"><?php echo $event['title'];?></h1>
-			<br>
-			<h2 id="type"><?php echo $event['type'];?></h2>
-		</div>
-		<h3 id="date">24/11/2015</h3>
-		<form id="option">
-			<select name="Option">
-				<option disabled selected> -- select an option -- </option>
-				<option value="going">Going</option>
-				<option value="not">Not Going</option>
-				<option value="maybe">Maybe</option>
-			</select>
-		</form>
-	</div>
-
-	<div id="description">
-		<h4>Description</h4>
+</head>
+<body>	
+	<div id="profile">
+		<h2>Username</h2>
 		<br>
+		<h4>Description Of User </h4>
 		<p>
 			Bacon ipsum dolor amet bresaola rump ribeye drumstick turkey tenderloin chicken prosciutto beef ham sausage pork belly. Tri-tip spare ribs swine beef ribs pork chop, tail capicola biltong turkey cupim venison corned beef hamburger bresaola. Ham drumstick tongue cupim, leberkas jerky capicola pork ground round corned beef ball tip shoulder sirloin pork chop venison. Shoulder pork fatback alcatra chicken pig. Pastrami pork chop porchetta drumstick t-bone sirloin flank andouille venison salami bacon hamburger beef ribs tongue pork. Corned beef frankfurter fatback, bresaola chuck porchetta sausage pastrami boudin salami cupim andouille.
 
@@ -43,37 +25,5 @@
 			Shank pancetta doner biltong andouille kielbasa jerky flank pork chicken porchetta fatback cupim jowl. Venison t-bone pork loin tongue boudin, shankle kielbasa ham tri-tip hamburger ball tip pastrami. Sausage beef ribs venison swine meatloaf pork belly pork chop pastrami. Landjaeger salami tongue, tenderloin doner meatloaf tri-tip venison pork meatball.
 		</p>
 	</div>
-
-	<div class="users">
-		<div id="users-going">
-			<h1>Going</h1>
-			<a href="">User1</a>
-			<br>
-			<a href="">User2</a>
-			<br>
-			<a href="">User3</a>
-			<br>
-			<a href="">User4</a>
-		</div>
-		<div id="users-not-going">
-			<h1>Not going</h1>
-			<a href="">User1</a>
-			<br>
-			<a href="">User2</a>
-			<br>
-			<a href="">User3</a>
-			<br>
-			<a href="">User4</a>
-		</div>
-		<div id="users-invited">
-			<h1>Invited</h1>
-			<a href="">User1</a>
-			<br>
-			<a href="">User2</a>
-			<br>
-			<a href="">User3</a>
-			<br>
-			<a href="">User4</a>
-		</div>
-	</div>
-</div>
+</body>
+</html>
