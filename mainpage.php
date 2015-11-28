925a3afb72238af78aa7740546c9bb267eee0331
444Duarte <duartemr.pinto@gmail.com>
2015-11-28 01:08:57 +0000

    Relative paths in mainpage.php

--- mainpage.php
+++ mainpage.php
@@ -3,9 +3,9 @@
 <head>
 	<title>Event Manager</title>
 	<meta charset='UTF-8'>
-	<!--<script src="dist/sweetalert.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="dist/sweetalert.css"> -->
-	<link rel="stylesheet" href="css/mainpage.css">
+	<script src="dist/sweetalert.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="dist/sweetalert.css">
+	<link rel="stylesheet" href="../css/mainpage.css">
 
 </head>
 <body>	
