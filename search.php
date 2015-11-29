f72435d17b221e10b09ef4a71b7e55bb5da0f01f
444Duarte <duartemr.pinto@gmail.com>
2015-11-29 23:29:24 +0000

    Search header finished

---
+++ search.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Search</title>
+	<meta charset='UTF-8'>
+	<script src="dist/sweetalert.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="dist/sweetalert.css">
+	<link rel="stylesheet" href="../css/mainpage.css">
+</head>
+
+<?php
+	include_once 'database/connect.php';
+	include_once 'database/access_db.php';
+
+
+	$idUser = $_GET['user'];
+	$searchQuery = urlencode($_GET['query']);
+?>
+
+<body>	
+	<?php
+		include 'templates/topbar.php'; 
+		include 'templates/leftbar.php';
+		include 'templates/search_results.php'; 
+	?>
+</body>
+</html>
---
+++ templates/search_results.php
@@ -0,0 +1,18 @@
+<head>
+	<link rel="stylesheet" href="../css/search_results.css">
+</head>
+
+
+
+<div class="display_search">
+	<div class="header_search">
+		<h1>Search</h1>
+		<h5 id="search_text">Search results for: 
+			<i><?php echo htmlentities(urldecode($searchQuery))?> </i>
+		</h5>
+	</div>
+
+	<div id="search_results">
+		
+	</div>
+</div>
