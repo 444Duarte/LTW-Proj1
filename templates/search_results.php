926b0ef5be456152291cf70f6d9294e93ba8127a
444Duarte <duartemr.pinto@gmail.com>
2015-11-30 00:18:48 +0000

    html básico acabado, começado o css

--- templates/search_results.php
+++ templates/search_results.php
@@ -13,6 +13,11 @@
 	</div>
 
 	<div id="search_results">
-		
+		<div class = "result_event">
+			<?php $event1 =  getEventByID(0);?>
+			<img class="event_image" src="<?php echo  '../' . $event1['image'];?>">
+			<a href="../mainpage.php/?event=<?php echo $event1['idEvent']; ?>  "><h1><?php echo $event1['title']; ?></h1></a>
+			<h3><?php echo $event1['eventDate']; ?>/h3>
+		</div>
 	</div>
 </div>
