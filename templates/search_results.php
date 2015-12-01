78446d1b73c61afd2f94332a8314d986ec81d5b3
444Duarte <duartemr.pinto@gmail.com>
2015-12-01 17:00:53 +0000

    Search- Layout feito

--- templates/search_results.php
+++ templates/search_results.php
@@ -16,8 +16,8 @@
 		<div class = "result_event">
 			<?php $event1 =  getEventByID(0);?>
 			<img class="event_image" src="<?php echo  '../' . $event1['image'];?>">
-			<a href="../mainpage.php/?event=<?php echo $event1['idEvent']; ?>  "><h1><?php echo $event1['title']; ?></h1></a>
-			<h3><?php echo $event1['eventDate']; ?></h3>
+			<a class="event_name" href="../mainpage.php/?event=<?php echo $event1['idEvent']; ?>  "><h1><?php echo $event1['title']; ?></h1></a>
+			<h3 class="event_date"><?php echo $event1['eventDate']; ?></h3>	
 		</div>
 	</div>
 </div>
