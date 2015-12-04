b1884a098d5dd6fb8892b4043dc0dd6ff7f56e0c
444Duarte <duartemr.pinto@gmail.com>
2015-12-04 16:29:19 +0000

    Eventos já funcionam, já só falta meio de apagar

--- templates/comments_section.php
+++ templates/comments_section.php
@@ -6,15 +6,15 @@
 
 
 <div id="comments_section">
-	<form id="user_make_comment" action="" method="post" >
+	<form id="user_make_comment" action="../php/comment.php" method="post" >
 			<input type="text" name="event" value="<?php echo $idEvent; ?>" hidden="true" >
 			<input type="text" name="user" value="<?php echo $idUser; ?>" hidden="true">
 			<?php if(userCanComment($idUser)) {?>
-				<textarea id="user_comment" rows="4" cols="25" maxlength="200" placeHolder="Place your comment here!"></textarea>
+				<textarea id="user_comment" name="comment" rows="4" cols="25" maxlength="200" placeHolder="Place your comment here!"></textarea>
 				<br>
 				<input class="sendCommentButton" type="submit" value="comment">
 			<?php }else{?>
-				<textarea id="user_comment" rows="4" cols="25" maxlength="200" disabled="true"
+				<textarea id="user_comment" name="comment" rows="4" cols="25" maxlength="200" disabled="true"
 				placeHolder="You need to be attending the event to be able to comment!"></textarea>
 				<br>
 				<input class="sendCommentButton" type="submit" value="comment" disabled="true">
@@ -28,9 +28,10 @@
 			<?php foreach($eventComments as $event){ ?>
 				<div class="user_comment">
 					<div>
-						<h5><?php echo getUserByID($event['idUser'])['idUser']; ?></h5>
+						<h5 class="username_comment"><?php echo getUserByID($event['idUser'])['user']; ?></h5>
 						<h6 class="date_comment"><?php echo $event['dateComment']; ?></h6>
 					</div>
+					<br>
 					<p><?php echo $event['comment']; ?></p>
 				</div>
 			<?php } ?>
