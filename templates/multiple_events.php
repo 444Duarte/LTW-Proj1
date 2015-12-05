ddfa0af948a78f8ca19667be04c51c18ab2d2016
444Duarte <duartemr.pinto@gmail.com>
2015-12-05 18:49:50 +0000

    Small path fix

--- templates/multiple_events.php
+++ templates/multiple_events.php
@@ -11,7 +11,7 @@
 	<div class = "single_event">
 		<?php $event =  getEventByID($idEvent['idEvent']);?>
 		<img class="event_image" src="<?php echo $event['image'];?>">
-		<a class="event_name" href="mainpage.php/?event=<?php echo $event['idEvent']; ?>  "><h1><?php echo $event['title']; ?></h1></a>
+		<a class="event_name" href="mainpage.php?event=<?php echo $event['idEvent']; ?>  "><h1><?php echo $event['title']; ?></h1></a>
 		<h3 class="event_date"><?php echo $event['eventDate']; ?></h3>	
 	</div>
 	<?php } ?>
