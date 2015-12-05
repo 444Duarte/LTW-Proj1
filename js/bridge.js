5826909abdec340858e232cfa2748581988fcb89
tomislaaaav <m_botelho8@hotmail.com>
2015-12-05 17:42:46 +0000

    js mudado para dar

--- js/bridge.js
+++ js/bridge.js
@@ -72,7 +72,7 @@ $("#LogForm").submit(function(ev){
                     break;
                 case 'success':
                     swal("Login successfull.", "Success.");
-                    window.location.href="profile.php";
+                    window.location.href="mainpage.php/?event=0&user=0";
                     break;
                 default:
                     //displayError("Error while processing the login...");
