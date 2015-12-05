8b9b90da749c0fe5bff4fe573e2a91c7963394d3
444Duarte <duartemr.pinto@gmail.com>
2015-12-05 22:59:55 +0000

    Perfil acabado

--- profile.php
+++ profile.php
@@ -20,15 +20,16 @@
         include 'templates/topbar.php';
         include 'templates/leftbar.php';
     ?>
-    <div id="tela">
+    <div id="profile">
         <h1>User Profile</h1>
         <div id="conteudo">
-            <div id="userphoto">
-                <img src ="<?php  echo $profile['image']; ?>"/>
-            </div>
-            <h2><?php echo $user['user']; ?></h2>
-    
+            <section id="profile_header">
+                <img id="userphoto" src ="<?php  echo $profile['image']; ?>"/>
+                <h2><?php echo $user['user']; ?></h2>
+            </section>
+            <br>
             <section id="bio">
+                <h3>Description</h3>
                 <p><?php echo $profile['description']; ?></p>
             </section>
         </div>
