8bcb4513ed868878d70a53693c1c04cbc1f7be58
444Duarte <duartemr.pinto@gmail.com>
2015-12-05 01:04:23 +0000

    TOPBAR- added create event

--- templates/topbar.php
+++ templates/topbar.php
@@ -5,8 +5,13 @@
 <div class="top-bar">
 		<button id="preferences_button" type="button"> <img src="../images/template/gear-16.png"></button>
 		<div id="pref_menu">
-			<a href="">Logout</a>
+			<a href="../php/logout.php">Logout</a>
+			<a href="#">About</a>
 		</div>
+		<button id="create_event_button" href="" type="button">
+			<img src="../images/template/plus_16.png">
+			<p>Create Event</p>
+		</button>
 		<button id="home_button" type="button"><img src="../images/template/logo-small.png"></button>
 		<form id="searchForm" action="../search.php/">
 			<input id="search-bar" type="text" name="query" required="optional" placeholder="Search"/>
