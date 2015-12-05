b7fab8a18e8a13ae10471c271a5030a9719a1276
Pedro Silva <pedro.mvsilva@gmail.com>
2015-12-05 18:07:02 +0000

    Nova disposição

--- mainpage.php
+++ mainpage.php
@@ -22,10 +22,8 @@
 	
 		include 'templates/topbar.php';
 		include 'templates/leftbar.php';
-		/* 
-		include 'templates/event.php'; 
-*/
-		include 'templates/perfil.php'; 
+		
+		include 'templates/event.php';  
 	?>
 </body>
 </html>
