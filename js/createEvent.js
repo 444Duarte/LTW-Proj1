d04512e0f8fbce65cdf2d06912a8cff4c8b72aa5
444Duarte <duartemr.pinto@gmail.com>
2015-12-06 21:37:49 +0000

    Já aparece a janela

---
+++ js/createEvent.js
@@ -0,0 +1,9 @@
+function closeCreateEvent() {
+	$('#create_form').hide();
+}
+
+
+function openCreateEvent () {
+	$('#create_form').show();
+	$('#create_form').focus();
+}
--- templates/createEvent.php
+++ templates/createEvent.php
@@ -1,72 +1,68 @@
-<!DOCTYPE html>
-<html>
-
 <?php
-	include_once '../database/access_db.php';
-	include_once '../database/connect.php';
+	include_once 'database/access_db.php';
+	include_once 'database/connect.php';
 
 	$types = retrieveAllEventTypes();
 ?>
 
+<head>
+	<link rel="stylesheet" href="css/createEvent.css">
+</head>
 
-	<head>
-	    <meta charset='UTF-8'>
-		<link rel="stylesheet" href="../css/createEvent.css">
-		<script src="../dist/sweetalert.min.js"></script>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
-	</head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
+<script src="js/createEvent.js"></script>
+<script src="js/bridge.js"></script>
 
-	<body>
-		<div id="create_form">
-			<header id="header" class="info">
-				<h1>Create Event</h1>
-			</header>
+<div id="create_form">
+	<button id="close_create_event" onClick="closeCreateEvent()" type="button">
+			<img src="images/template/x_button.png">
+	</button>
 
-			<form id="create" type="submit" method="post" action="" enctype="multipart/form-data">	
-				<label for="title">Title:</label>
-				<input type="text" id="title" name="title">
-				<br>
+	<header id="header" class="info">
+		<h1>Create Event</h1>
+	</header>
 
-				<label for="date">Date:</label>
-				<input type="date" id="date" name="date">
-				<br>
-				
-				<label for="description">Description: </label>
-				<br>
-				<textarea rows="3" cols="30" id="description" name="description"></textarea>
-				<br>
+	<form id="create" type="submit" method="post" action="" enctype="multipart/form-data">	
+		<label for="title">Title:</label>
+		<input type="text" id="title" name="title">
+		<br>
 
-				<label for="image">Image: </label>
-				<input type="file" id="image" name="image">
-				<br>
+		<label for="date">Date:</label>
+		<input type="date" id="date" name="date">
+		<br>
+		
+		<label for="description">Description: </label>
+		<br>
+		<textarea rows="3" cols="30" id="description" name="description"></textarea>
+		<br>
 
-				<label for="Privacy">Privacy: </label>
-				<select id="Privacy" name="Privacy">
-					<option value="Public" selected="Selected">Public</option>
-					<option value="Private">Private</option>
-				</select>
-				<br>
+		<label for="image">Image: </label>
+		<input type="file" id="image" name="image">
+		<br>
 
-				<label for="Type">Type: </label>
-				<select id="Type" name="Type">
+		<label for="Privacy">Privacy: </label>
+		<select id="Privacy" name="Privacy">
+			<option value="Public" selected="Selected">Public</option>
+			<option value="Private">Private</option>
+		</select>
+		<br>
+
+		<label for="Type">Type: </label>
+		<select id="Type" name="Type">
+			<?php
+				foreach($types as $row) {
+			?>
+				<option value="<?php echo $row['type'];?>" >
 					<?php
-						foreach($types as $row) {
+						echo $row['type'];
 					?>
-						<option value="<?php echo $row['type'];?>" >
-							<?php
-								echo $row['type'];
-							?>
-						</option>
-					<?php
-						}
-					?>
-				</select>
-				<br>
+				</option>
+			<?php
+				}
+			?>
+		</select>
+		<br>
 
-				<button id="criar" type="submit">Criar Evento</button>
-			</form>
-		</div>
-	</body>
-
-	<script src="./../js/bridge.js"></script>
-</html>
+		<button id="criar" type="submit">Criar Evento</button>
+	</form>
+</div>
